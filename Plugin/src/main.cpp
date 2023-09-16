5a34e7b1bd17073997ce831bec47f45198c39521
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2023-09-16 11:43:46 -0600

    fix trampoline alloc size

--- Plugin/src/main.cpp
+++ Plugin/src/main.cpp
@@ -136,7 +136,7 @@ DLLEXPORT bool SFSEAPI SFSEPlugin_Load(const SFSE::LoadInterface* a_sfse)
 
 	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
 
-	SFSE::AllocTrampoline(1 << 6);
+	SFSE::AllocTrampoline(1 << 4);
 
 	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
 
